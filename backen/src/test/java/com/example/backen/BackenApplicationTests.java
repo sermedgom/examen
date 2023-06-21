d130b3c6a0ad3abab9ab55192ed09a8df167f7b7
Sergio <sergiomedinagomez2014@gmail.com>
2023-06-21 23:06:00 +0200

    subir cosas

---
+++ backen/src/test/java/com/example/backen/BackenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
